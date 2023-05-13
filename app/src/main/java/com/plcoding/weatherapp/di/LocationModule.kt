fbb36db94954f00565040de4e053cfeb987539d6
Muhammad Ali <alisoomro493@gmail.com>
2023-05-14 01:15:12 +0500

    Added Dagger Hilt module for providing dependencies related to location tracking and Repository

---
+++ app/src/main/java/com/plcoding/weatherapp/di/LocationModule.kt
@@ -0,0 +1,21 @@
+package com.plcoding.weatherapp.di
+
+import com.plcoding.weatherapp.data.remote.location.DefaultLocationTracker
+import com.plcoding.weatherapp.domain.location.LocationTracker
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import javax.inject.Singleton
+
+@ExperimentalCoroutinesApi
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocationModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindLocationTracker(defaultLocationTracker: DefaultLocationTracker): LocationTracker
+
+}
